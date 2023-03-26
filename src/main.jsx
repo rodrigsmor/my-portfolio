8c51d225fcb72794af5f3a30a371929e50c1c544
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2023-03-25 21:48:39 -0300

    Started to setup project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import React from 'react'
+import { theme } from './style/theme'
+import ReactDOM from 'react-dom/client'
+import GlobalStyle from './style/globalStyle'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/style/theme.js
@@ -0,0 +1,42 @@
+export const theme = {
+  colors: {
+    primary: {
+      main: '#865DFF',
+      dark: ''
+    },
+    secondary: {
+      main: '#E384FF',
+      dark: ''
+    },
+    surface: {
+      main: '#2B1D52',
+      dark: '#231842'
+    },
+    typography: {
+      main: '#DBD3F0'
+    },
+    background: {
+      main: '#130D24'
+    }
+  },
+  typography: {
+    type: {
+      main: '',
+      logo: ''
+    },
+    size: {
+      h6: '14px',
+      h5: '16px',
+      h4: '20px',
+      h3: '24px',
+      h2: '36px',
+      h1: '48px'
+    },
+    weight: {
+
+    }
+  },
+  shadow: {
+
+  }
+}
