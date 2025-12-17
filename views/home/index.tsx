ac811c36a1c1c128905dab339024fc73ef997132
Rodrigo Moreira da Silva <rodrigomoreiradasilva.of@gmail.com>
2025-12-16 23:08:15 -0300

    :sparkles: feat: add translations to home page

--- views/home/index.tsx
+++ views/home/index.tsx
@@ -5,33 +5,35 @@ import { Button } from "@/components/buttons/button";
 import { ArrowUpRight, RocketLaunch } from "phosphor-react";
 import { SocialLinks } from "@/components/layout/social-links";
 import { ScrollDownIndicator } from "@/components/textual/scroll-down-indicator";
+import { useTranslate } from "@/utils/hooks/useTranslate";
 
 export function Home() {
+  const { t } = useTranslate();
   return (
     <section className={`${styles.home}`}>
       <header className={styles.homeContent}>
         <h1 className={styles.homeGreetings}>
-          Hello, I’m a
+          {t('Home.Title.greetings')}
           <strong className="uppercaseText">
-            Software
-            <span>Developer</span>
+            {t('Home.Title.role')}
+            <span>{t('Home.Title.highlightRole')}</span>
           </strong>
           <span className={styles.secondaryRoleWrapper}>
             <span className="minorCoordinating">&amp; </span>
-            <span className="secondaryRole">UI/UX Designer</span>
+            <span className="secondaryRole">{t('Home.Title.secondaryRole')}</span>
           </span>
         </h1>
         <div className={styles.separatorLine}></div>
         <p className={styles.homeDescription}>
-          I look forward to creating innovative and reliable solutions with you. Explore my portfolio and discover how I can bring value to your project.
+          {t('Home.Description')}
         </p>
       </header>
       <div className={styles.ctaGroup}>
         <Button theme="gradient">
-          <RocketLaunch width={24} height={24} weight="bold" /> Let’s Work Together
+          <RocketLaunch width={24} height={24} weight="bold" /> {t('Home.PrimaryButton')}
         </Button>
         <Button theme="outline">
-          View My Projects<ArrowUpRight width={24} height={24}weight="bold" />
+          {t('Home.SecondaryButton')}<ArrowUpRight width={24} height={24}weight="bold" />
         </Button>
       </div>
       <footer className={styles.footerSocialMedia}>
